2dd57abed989873867fa5d8b777c281647a04487
JuanSolisA <juan.solis.trabajo@gmail.com>
2022-06-21 20:58:17 -0300

    Iniciando carrito

--- app.js
+++ app.js
@@ -18,3 +18,7 @@ app.get('/login', (req,res)=>{
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
+
+app.get('/cart', (req,res)=>{
+    res.sendFile(__dirname + '/views/cart.html');
+});
