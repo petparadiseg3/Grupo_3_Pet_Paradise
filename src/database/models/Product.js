0c23fe0ee12dc00eb1b551d50c402bb8dcbebc75
JuanSolisA <juan.solis.trabajo@gmail.com>
2022-09-02 00:19:26 -0300

    finalizando-las-relaciones

--- src/database/models/Product.js
+++ src/database/models/Product.js
@@ -1,23 +1,50 @@
-module.exports = (sequelize, dataTypes) => {  
-    const Product = sequelize.define("Product", {
-        id:{
-            type: dataTypes.INTEGER, 
-            primaryKey: true, 
-            autoIncrement: true 
-        },
-        name:{
-            type:dataTypes.STRING,
-        },
-        description:{
-           type: dataTypes.TEXT
-        },
-        image:{
-            type: dataTypes.STRING
-        }
-    },{
-        tableName: 'product',
-        timestamps: false
-    });  //El 1er parametro de define es el nombre de la entidad, el 2do es el objeto de las columnas, el 3er parametro recibe el nombre de la tabla (como sale en la base de dato mysql)y timestamps
+module.exports = (sequelize, dataTypes) => {
+  const Product = sequelize.define(
+    "Product",
+    {
+      id: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: dataTypes.STRING,
+      },
+      descriptions: {
+        type: dataTypes.TEXT,
+      },
+      image: {
+        type: dataTypes.STRING,
+      },
+    },
+    {
+      tableName: "product",
+      timestamps: false,
+    }
+  ); //El 1er parametro de define es el nombre de la entidad, el 2do es el objeto de las columnas, el 3er parametro recibe el nombre de la tabla (como sale en la base de dato mysql)y timestamps
 
-    return Product; 
-}
+  Product.associate = function (models) {
+    Product.hasMany(models.OrderDetail, {
+      as: "producto",
+      foreignKey: "product_id",
+    });
+
+    Product.belongsToMany(models.Weight, {
+      as: "pesos",
+      throw: "product_weight",
+      foreignKey: "product_id",
+      otherKey: "weight_id",
+      timestamps: false,
+    });
+
+    Product.belongsToMany(models.Category, {
+      as: "categorias",
+      throw: "product_categories",
+      foreignKey: "product_id",
+      otherKey: "category_id",
+      timestamps: false,
+    });
+  };
+
+  return Product;
+};
--- src/database/models/User.js
+++ src/database/models/User.js
@@ -16,9 +16,9 @@ module.exports = (sequelize, dataTypes) => {
       email: {
         type: dataTypes.STRING,
       },
-      password:{
-        type: dataTypes.STRING
-     },
+      password: {
+        type: dataTypes.STRING,
+      },
       address: {
         type: dataTypes.STRING,
       },
@@ -37,6 +37,13 @@ module.exports = (sequelize, dataTypes) => {
       timestamps: true,
     }
   ); //El 1er parametro de define es el nombre de la entidad, el 2do es el objeto de las columnas, el 3er parametro recibe el nombre de la tabla (como sale en la base de dato mysql)y timestamps
+
+  User.associate = function (models) {
+    User.hasMany(models.Order, {
+      as: "ordenes",
+      foreignKey: "user_id",
+    });
+  };
 
   return User;
 };
