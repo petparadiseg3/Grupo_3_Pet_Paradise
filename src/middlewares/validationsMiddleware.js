afc0a22fab330e01c783a4b5f860c3bff8f2fdc5
JuanSolisA <juan.solis.trabajo@gmail.com>
2022-10-11 14:38:16 -0300

    agregando validaciones del back

--- src/middlewares/validationsMiddleware.js
+++ src/middlewares/validationsMiddleware.js
@@ -2,44 +2,47 @@ const { body } = require("express-validator");
 const path = require("path");
 
 module.exports = [
-    body("name")
-        .trim()
-        .escape()
-        .notEmpty()
-        .withMessage("Tienes que escribir un nombre"),
-    body("surname")
-        .trim()
-        .escape()
-        .notEmpty()
-        .withMessage("Tienes que escribir tu apellido"),
-    body("email")
-        .notEmpty().withMessage("Tienes que escribir un email")
-        .bail()
-        .isEmail().withMessage("Debes escribir un formato de correo válido"),
-    body("phone")
-        .trim()
-        .escape()
-        .notEmpty()
-        .withMessage("Tienes que escribir un telefono"),
-    body("password")
-        .trim()
-        .escape()
-        .notEmpty()
-        .withMessage("Tienes que escribir una contraseña"),
-    body("picture_user").custom((value, { req }) => {
-        let file = req.file;
-        let acceptedExtensions = [".jpg", ".png", ".gif",".jpeg"];
-        if (!file) {
-            throw new Error("Tienes que subir una imagen");
-        } else {
-            let fileExtension = path.extname(file.originalname);
-            if (!acceptedExtensions.includes(fileExtension)) {
-                throw new Error(
-                    `Las extensiones permitidas son ${acceptedExtensions.join(", ")}`
-                );
-            }
-        }
-        return true;
-    }),
+  body("name").notEmpty().withMessage("Tienes que escribir un nombre").trim(),
+  body("surname")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Tienes que escribir tu apellido"),
+  body("email")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Tienes que escribir un email")
+    .bail()
+    .escape()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Debes escribir un formato de correo válido"),
+  body("phone")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Tienes que escribir un telefono"),
+  body("password")
+  .trim()
+  .escape()
+  .notEmpty()
+  .withMessage("Tienes que escribir una contraseña")
+  .isLength({ min: 8 })
+  .withMessage("La contraseña debe contener 8 caracteres"),
+  body("picture_user").custom((value, { req }) => {
+    let file = req.file;
+    let acceptedExtensions = [".jpg", ".png", ".gif", ".jpeg"];
+    if (!file) {
+      throw new Error("Tienes que subir una imagen");
+    } else {
+      let fileExtension = path.extname(file.originalname);
+      if (!acceptedExtensions.includes(fileExtension)) {
+        throw new Error(
+          `Las extensiones permitidas son ${acceptedExtensions.join(", ")}`
+        );
+      }
+    }
+    return true;
+  }),
 ];
-
