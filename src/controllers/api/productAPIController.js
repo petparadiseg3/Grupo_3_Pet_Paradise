1b1df71c2aa6263e1d5591a067c05a3fccadae12
MoiZuleta98 <moiseszuleta02@hotmail.com>
2022-10-21 01:10:38 -0300

    Api de lista de productos casi terminada, falta agregar una propiedad por categoria con el total de productos(realizado pero no con las propiedades)

--- src/controllers/api/productAPIController.js
+++ src/controllers/api/productAPIController.js
@@ -1,42 +1,70 @@
-const path = require('path');
-const db = require('../../database/models');
-const sequelize = db.sequelize;
+const path = require("path");
+const db = require("../../database/models");
+const Sequelize = db.sequelize;
 const { Op } = require("sequelize");
 
-const Products = db.Product;
+const Category = db.Category;
+const Product = db.Product;
+
+/* let productControllerAPI = {
+  buscador: async (req, res) => {
+    
+
+    try {
+      let allCategories = await Category.findAll();
+      let result = await Product.findAll({ where: {  } });
+
+      res.json(result);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
+
+module.exports = productControllerAPI; */
+const Productoo = db.Product.findAll()
 
 const productAPIController = {
-    'list': (req, res) => {
-        db.Product.findAll()
-        .then(products => {
-            let respuesta = {
-                meta: {
-                    status: 200,
-                    total: products.length,
-                    url: 'api/products'
-                },
-                data: products
-            }
-                res.json(respuesta);
+    
+
+    list: (req, res) => {
+        db.Product.findAll({
+            include: [
+                {
+                    model: db.Category, as: 'categoria', 
+                    attributes: ['name']
+                }],
+            
+
+
+        }).then(result => {
+            
+            return res.status(200).json(result.map((e)=> {
+                return {
+                    id: e.id,
+                    name: e.name,
+                    descriptions: e.descriptions,
+                    detail: "/api/products/" + e.id,
+                    categoria:e.categoria
+                }
+            }));
         })
     },
 
-    'detail': (req, res) => {
+    detail: (req, res) => {
         db.Product.findByPk(req.params.id)
-            .then(product => {
+            .then((product) => {
                 let respuesta = {
                     meta: {
                         status: 200,
-                        total: product.length,
-                        url: '/api/products/:id'
+                        url: "/api/products/:id",
                     },
-                    data: product
-                }
+                    data: product,
+                };
                 res.json(respuesta);
             })
+            .catch((error) => console.log(error));
+    },
+};
 
-    }
-
-}
-
-module.exports = productAPIController
+module.exports = productAPIController;
